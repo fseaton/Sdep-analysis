5129ab7d73f323ea5ff3db26cbecd3ced9e16e06
Fiona Seaton <fseaton@ceh.ac.uk>
2020-09-24 13:57:12 +0100

    Switch to odbc and DBI packages rather than RODBC

--- 01_Data_read.R
+++ 01_Data_read.R
@@ -54,64 +54,67 @@ Ndep_avg <- fread("N:/UKScape/CBED_1970-2018_SN_dep/totalCBED_Ndep_gridavg_1970-
 Ndep_for <- fread("N:/UKScape/CBED_1970-2018_SN_dep/totalCBED_Ndep_forest_1970-2018_5km_kgN_ha.csv")
 Ndep_moo <- fread("N:/UKScape/CBED_1970-2018_SN_dep/totalCBED_Ndep_moor_1970-2018_5km_kgN_ha.csv")
 
-library(RODBC)
 ## get data on species characteristics from CS database
 pwds <- read.csv("Outputs/pwd.csv")
-channel3 <- odbcConnect("FEGEN", uid="csgeo", pwd=pwds[pwds$uid=="csgeo","pwd"], believeNRows=FALSE)
-sqlTables(channel3, schema="CSVEG")
-channelg <- odbcConnect("tbb", uid="AXISII_WP3_VIEWER", 
-                        pwd=pwds[pwds$uid=="AXISII_WP3_VIEWER","pwd"], believeNRows=FALSE)
+library(odbc)
+library(DBI)
+FEGEN <- dbConnect(odbc(), "FEGEN", UID="csgeo", PWD=pwds[pwds$uid=="csgeo","pwd"])
+
+dbListTables(FEGEN, schema = "CSVEG")
+
+channelg <- dbConnect(odbc(), "tbb", UID="AXISII_WP3_VIEWER", 
+                        PWD=pwds[pwds$uid=="AXISII_WP3_VIEWER","pwd"])
 
 # derived IBD files
-CS78_IBD <- sqlFetch(channel3, "CSVEG.IBD78")
-CS90_IBD <- sqlFetch(channel3, "CSVEG.IBD90")
-CS98_IBD <- sqlFetch(channel3, "CSVEG.IBD98")
-CS07_IBD <- sqlFetch(channel3, "CSVEG.IBD07")
-GM16_IBD <- sqlFetch(channelg, "GMEP_DERIVED.WP6_IBD_YRS1234")
+CS78_IBD <- dbReadTable(FEGEN, SQL("CSVEG.IBD78"))
+CS90_IBD <- dbReadTable(FEGEN, SQL("CSVEG.IBD90"))
+CS98_IBD <- dbReadTable(FEGEN, SQL("CSVEG.IBD98"))
+CS07_IBD <- dbReadTable(FEGEN, SQL("CSVEG.IBD07"))
+GM16_IBD <- dbReadTable(channelg, SQL("GMEP_DERIVED.WP6_IBD_YRS1234"))
 
 # metadata
-SPECIES_LIB_TRAITS <- sqlFetch(channel3, "CSVEG.LUS_SP_LIB_AND_TRAITS")
-SPECIES_LIB_CODES <- sqlFetch(channel3, "CSVEG.LUS_SP_LIB_CODES_NEW")
-BHPH_NAMES <- sqlFetch(channel3, "CSVEG.BHPH_NAMES")
+SPECIES_LIB_TRAITS <- dbReadTable(FEGEN, SQL("CSVEG.LUS_SP_LIB_AND_TRAITS"))
+SPECIES_LIB_CODES <- dbReadTable(FEGEN, SQL("CSVEG.LUS_SP_LIB_CODES_NEW"))
+BHPH_NAMES <- dbReadTable(FEGEN, SQL("CSVEG.BHPH_NAMES"))
 
 # plant species records
-CS19_SP <- sqlFetch(channel3, "CSVEG.VEGETATION_PLOT_SP_161819") 
-CS78_SP <- sqlFetch(channel3, "CSVEG.CS78_SPECIESDATA")
-CS90_SP <- sqlFetch(channel3, "CSVEG.CS90_SPECIESDATA")
-CS98_SP <- sqlFetch(channel3, "CSVEG.CS9899_SPECIESDATA")
-CS07_SP <- sqlFetch(channel3, "CSVEG.VEGETATION_PLOT_SPECIES_2007")
+CS19_SP <- dbReadTable(FEGEN, SQL("CSVEG.VEGETATION_PLOT_SP_161819"))
+CS78_SP <- dbReadTable(FEGEN, SQL("CSVEG.CS78_SPECIESDATA"))
+CS90_SP <- dbReadTable(FEGEN, SQL("CSVEG.CS90_SPECIESDATA"))
+CS98_SP <- dbReadTable(FEGEN, SQL("CSVEG.CS9899_SPECIESDATA"))
+CS07_SP <- dbReadTable(FEGEN, SQL("CSVEG.VEGETATION_PLOT_SPECIES_2007"))
 
 # plot records
-VEGETATION_PLOTS_20161819 <- sqlFetch(channel3, "CSVEG.VEGETATION_PLOTS_20161819")
-CS07_PLOTS <- sqlFetch(channel3, "CSVEG.VEGETATION_PLOTS_2007")
-CS98_PLOTS <- sqlFetch(channel3, "CSVEG.CS9899_QUADS_DESCRIPTION")
-CS90_PLOTS <- sqlFetch(channel3, "CSVEG.CS90_QUADS_DESCRIPTION")
-CS78_PLOTS <- sqlFetch(channel3, "CSVEG.CS78_QUADS_DESCRIPTION")
+VEGETATION_PLOTS_20161819 <- dbReadTable(con, SQL("CSVEG.VEGETATION_PLOTS_20161819"))
+CS07_PLOTS <- dbReadTable(FEGEN, SQL("CSVEG.VEGETATION_PLOTS_2007"))
+CS98_PLOTS <- dbReadTable(FEGEN, SQL("CSVEG.CS9899_QUADS_DESCRIPTION"))
+CS90_PLOTS <- dbReadTable(FEGEN, SQL("CSVEG.CS90_QUADS_DESCRIPTION"))
+CS78_PLOTS <- dbReadTable(FEGEN, SQL("CSVEG.CS78_QUADS_DESCRIPTION"))
 
 # soils data
-channel2 <- odbcConnect("MWA", uid="masq", pwd=pwds[pwds$uid=="masq","pwd"], believeNRows=FALSE)
-sqlTables(channel2, schema="DB_MASQ")
+MWA_masq <- dbConnect(odbc(), "MWA", UID="masq", 
+                      PWD=pwds[pwds$uid=="masq","pwd"])
 
 # pH data
-CS16_PH <- sqlFetch(channel2, "DB_MASQ.CS2016_PH_LOI_DATA")
-CS07_PH <- sqlFetch(channel2, "DB_MASQ.CS2007_PH_LOI_DATA")
-CS78_PH <- sqlFetch(channel2, "DB_MASQ.CS1978_PH_LOI_DATA")
-CS98_PH <- sqlFetch(channel2, "DB_MASQ.CS1998_PH_LOI_DATA")
+CS16_PH <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS2016_PH_LOI_DATA"))
+CS07_PH <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS2007_PH_LOI_DATA"))
+CS78_PH <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS1978_PH_LOI_DATA"))
+CS98_PH <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS1998_PH_LOI_DATA"))
 UK19_PH <- read.csv("Outputs/UK19_PHLOI.csv")
 
-CS07_CN <- sqlFetch(channel2, "DB_MASQ.CS2007_CN_DATA")
-CS98_CN <- sqlFetch(channel2, "DB_MASQ.CS1998_CN_DATA")
+CS07_CN <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS2007_CN_DATA"))
+CS98_CN <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS1998_CN_DATA"))
 
 # tier 4 data
-CS_tier4 <- sqlFetch(channel2, "DB_MASQ.CS_SOILS_TIER_4_DATA")
+CS_tier4 <- dbReadTable(MWA_masq, SQL("DB_MASQ.CS_SOILS_TIER_4_DATA"))
 
 # landclass data
-channel <- odbcConnect("MWA", uid = "lus_user", pwd = pwds[pwds$uid=="lus_user","pwd"], 
-                       case = "nochange", believeNRows = FALSE)
-sqlTables(channel, schema = "DB_CSSQUARES")
-landclass_dat <- sqlFetch(channel, "DB_CSSQUARES.SQUARES_FILE_ALL_LC")
-plot_locations <- sqlFetch(channel, "DB_CSSQUARES.SQUARE_AND_PLOT_LOCATIONS")
-plot_dates <- sqlFetch(channel, "DB_CSSQUARES.DATEVARS_78_84_90_98_07")
+MWA_lus <- dbConnect(odbc(), "MWA", UID="lus_user", 
+                           PWD=pwds[pwds$uid=="lus_user","pwd"])
+
+landclass_dat <- dbReadTable(MWA_lus, SQL("DB_CSSQUARES.SQUARES_FILE_ALL_LC"))
+plot_locations <- dbReadTable(MWA_lus, SQL("DB_CSSQUARES.SQUARE_AND_PLOT_LOCATIONS"))
+plot_dates <- dbReadTable(MWA_lus, SQL("DB_CSSQUARES.DATEVARS_78_84_90_98_07"))
 
 # CEH laptop data
 UK19_WET <- read_excel("C:/Users/fseaton/Documents/CS/Copy of UKSCAPE Field Survey 2019 CHEMICAL DARv3.xls",
