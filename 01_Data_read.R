0dcf315acd00b46a4483fbba7987827d18c848ae
Fiona Seaton <fseaton@ceh.ac.uk>
2020-10-06 17:15:07 +0100

    Removal of methods for reading data in from laptop no longer in use

--- 01_Data_read.R
+++ 01_Data_read.R
@@ -2,49 +2,6 @@
 library(data.table)
 library(readxl)
 
-# Laptop
-# Atmospheric data
-Sdep_avg <- fread("../CBED_1970-2018_SN_dep/totalCBED_Sdep_gridavg_1970-2018_5km_kgS_ha.csv")
-Sdep_for <- fread("../CBED_1970-2018_SN_dep/totalCBED_Sdep_forest_1970-2018_5km_kgS_ha.csv")
-Sdep_moo <- fread("../CBED_1970-2018_SN_dep/totalCBED_Sdep_moor_1970-2018_5km_kgS_ha.csv")
-
-Ndep_avg <- fread("../CBED_1970-2018_SN_dep/totalCBED_Ndep_gridavg_1970-2018_5km_kgN_ha.csv")
-Ndep_for <- fread("../CBED_1970-2018_SN_dep/totalCBED_Ndep_forest_1970-2018_5km_kgN_ha.csv")
-Ndep_moo <- fread("../CBED_1970-2018_SN_dep/totalCBED_Ndep_moor_1970-2018_5km_kgN_ha.csv")
-
-# Soils data
-CS78_PH <- fread("../../../CS/Data/CS1978_PH_LOI_DATA.csv")
-CS98_PH <- fread("../../../CS/Data/CS1998_PH_LOI_DATA.csv")
-CS07_PH <- fread("../../../CS/Data/CS2007_PH_LOI_DATA.csv")
-CS16_PH <- fread("../../../CS/Data/CS2016_PH_LOI_DATA.csv")
-UK19_PH <- fread("../../UK19_PHLOI.csv")
-
-CS_tier4 <- fread("../../../CS/Data/CS_SOILS_TIER_4_DATA.csv")
-UK19_WET <- read_excel("../../Copy of UKSCAPE Field Survey 2019 CHEMICAL DARv3.xls",
-                       sheet = "DATA", skip = 3, na = "NA")
-
-# veg data
-CS78_IBD <- fread("../../../CS/Data/IBD78.csv")
-CS90_IBD <- fread("../../../CS/Data/IBD90.csv")
-CS98_IBD <- fread("../../../CS/Data/IBD98.csv")
-CS07_IBD <- fread("../../../CS/Data/IBD07.csv")
-GM16_IBD <- fread("../../../CS/Data/GMEP_VEG_IBD_XNEST1.csv")
-
-# Plant species recordS
-CS19_SP <- fread("../../../CS/Data/VEGETATION_PLOT_SP_161819.csv")
-CS07_SP <- fread("../../../CS/Data/VEGETATION_PLOT_SPECIES_2007.csv")
-CS98_SP <- fread("../../../CS/Data/CS9899_SPECIESDATA.csv")
-CS90_SP <- fread("../../../CS/Data/CS90_SPECIESDATA.csv")
-CS78_SP <- fread("../../../CS/Data/CS78_SPECIESDATA.csv")
-
-# metadata
-BHPH_NAMES <- read.csv("../../../CS/Data/BHPH_names.csv")
-CS07_PLOTS <- read.csv("../../../CS/Data/VEGETATION_PLOTS_2007.csv")
-landclass_dat <- read.csv("../../../CS/Data/CS_landclass_dat.csv")
-SPECIES_LIB_TRAITS <- fread("../../../CS/Data/SPECIES_LIB_TRAITS.csv")
-SPECIES_LIB_CODES <- fread("../../../CS/Data/SPECIES_LIB_CODES.csv")
-
-# Desktop
 # atmospheric deposition data
 Sdep_avg <- fread("N:/UKScape/CBED_1970-2018_SN_dep/totalCBED_Sdep_gridavg_1970-2018_5km_kgS_ha.csv")
 Sdep_for <- fread("N:/UKScape/CBED_1970-2018_SN_dep/totalCBED_Sdep_forest_1970-2018_5km_kgS_ha.csv")
@@ -137,5 +94,5 @@ plot_locations <- dbReadTable(MWA_lus, SQL("DB_CSSQUARES.SQUARE_AND_PLOT_LOCATIO
 plot_dates <- dbReadTable(MWA_lus, SQL("DB_CSSQUARES.DATEVARS_78_84_90_98_07"))
 
 # CEH laptop data
-UK19_WET <- read_excel("C:/Users/fseaton/Documents/CS/Copy of UKSCAPE Field Survey 2019 CHEMICAL DARv3.xls",
-                       sheet = "DATA", skip = 3, na = "NA")
+UK19_WET <- read_excel("Outputs/UKSCAPE Field Survey 2019 CHEMICAL DARv5.xls",
+                       sheet = "DATA clean", na = c("NA",-9999))
